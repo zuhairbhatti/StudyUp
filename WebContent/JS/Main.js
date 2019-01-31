6751b0081dd021670f0a44b913bac8e3c3fadddc
Zuhair Bhatti <zqbhatti@ucdavis.edu>
2019-01-30 19:41:11 -0800

    Update client

--- WebContent/JS/Main.js
+++ WebContent/JS/Main.js
@@ -3,10 +3,10 @@ initMap()
 
 function initMap() {
 	window.view = new ol.View({
-		center: ol.proj.fromLonLat([-121.751392674913, 38.52247515]),
+		center: ol.proj.fromLonLat([-121.751392674913, 38.52247515]), // Use Davis, CA as default coordinates
 		zoom: 13
 	})
-	window.map = new ol.Map({
+	map = new ol.Map({
 		target: 'map',
 		layers: [
 			new ol.layer.Tile({
@@ -15,10 +15,43 @@ function initMap() {
 		],
 		view: window.view
 	});
+	map.on("moveend", onMoveEnd);
 }
 
-function gotoLoc(lat, lon) {
-	const coords = ol.proj.fromLonLat([lon, lat]);
-	window.view.animate({center: coords, zoom: 13});
-//	window.view.fit(ol.proj.transformExtent([minLon, minLat, maxLon, maxLat], 'EPSG:4326', 'EPSG:3857'))
+
+function gotoLoc(lat, lon, bounds) {
+	const coordinates = ol.proj.fromLonLat([lon, lat]);
+	window.view.animate({center: coordinates, zoom: 13});
+	if (bounds) {
+		window.view.fit(ol.proj.transformExtent(bounds, 'EPSG:4326', 'EPSG:3857'), window.map.getSize())
+	}
+}
+
+// These two functions based on https://openlayers.org/en/latest/examples/moveend.html
+function wrapLon(value) {
+	var worlds = Math.floor((value + 180) / 360);
+	return value - (worlds * 360);
+}
+
+function onMoveEnd(e) {
+	var map = e.map;
+    var extent = map.getView().calculateExtent(map.getSize());
+    var bottomLeft = ol.proj.toLonLat(ol.extent.getBottomLeft(extent));
+    var topRight = ol.proj.toLonLat(ol.extent.getTopRight(extent));
+    minLon = bottomLeft[1];
+    minLat = wrapLon(bottomLeft[0]);
+    maxLon = topRight[1];
+    maxLat = wrapLon(topRight[0]);
+    bounds = [minLon, minLat, maxLon, maxLat];
+    bounds = minLon + "," + minLat + "," + maxLon + "," + maxLat;
+    $.ajax({
+		url: 'MoveServlet',
+		data: {
+			bounds: bounds
+		},
+		success: function(responseText) {
+			console.log(responseText);
+		}
+	});
+    console.log(bounds);
 }
