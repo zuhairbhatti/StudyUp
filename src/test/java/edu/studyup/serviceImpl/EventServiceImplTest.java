25f1b77a7e709c30b8a2b1b835b4c54943094025
shivani <shivani@beehyv.com>
2019-01-31 13:22:34 -0800

    Added basic testcases for EventServiceImpl

---
+++ src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
@@ -0,0 +1,82 @@
+package edu.studyup.serviceImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import edu.studyup.entity.Event;
+import edu.studyup.entity.Location;
+import edu.studyup.entity.Student;
+import edu.studyup.util.DataStorage;
+import edu.studyup.util.StudyUpException;
+
+class EventServiceImplTest {
+
+	EventServiceImpl eventServiceImpl;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		eventServiceImpl = new EventServiceImpl();
+		//Create Student
+		Student student = new Student();
+		student.setFirstName("John");
+		student.setLastName("Doe");
+		student.setEmail("JohnDoe@email.com");
+		student.setId(1);
+		
+		//Create Event1
+		Event event = new Event();
+		event.setEventID(1);
+		event.setDate(new Date());
+		event.setName("Event 1");
+		Location location = new Location(-122, 37);
+		event.setLocation(location);
+		List<Student> eventStudents = new ArrayList<>();
+		eventStudents.add(student);
+		event.setStudents(eventStudents);
+		
+		DataStorage.eventData.put(event.getEventID(), event);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		DataStorage.eventData.clear();
+	}
+
+	@Test
+	void testUpdateEventName_GoodCase() {
+		Event event = new Event();
+		event.setEventID(1);
+		event.setName("Renamed Event 1");
+		eventServiceImpl.updateEvent(event);
+		assertEquals("Renamed Event 1", DataStorage.eventData.get(event.getEventID()).getName());
+	}
+	
+	@Test
+	@Disabled
+	void testUpdateEvent_badCase() {
+		Event event = null;
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.updateEvent(event);
+		  });
+	}
+
+}
