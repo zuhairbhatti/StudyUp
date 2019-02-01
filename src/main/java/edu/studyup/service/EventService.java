1ba9f98cb8c9d0ab314a251b98399a2b0bfb101b
shivani <shivani@beehyv.com>
2019-02-01 12:59:03 -0800

    Homework 2 Final changes.

--- src/main/java/edu/studyup/service/EventService.java
+++ src/main/java/edu/studyup/service/EventService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import edu.studyup.entity.Event;
 import edu.studyup.entity.Student;
+import edu.studyup.util.StudyUpException;
 
 /**
  * {@code EventService} holds all CRUD services for class {@link Event}
@@ -14,42 +15,44 @@ import edu.studyup.entity.Student;
 public interface EventService {
 
 	/**
-	 * @param event The specific {@link Event} to be updated.
-	 * @return The updated {@code event}
+	 * Updates the event name by taking in an eventID and the new name.
+	 * throws error if any failed constraints.
+	 * @param eventID The ID of the specific {@link Event} to be updated.
+	 * @param name    The String value to update.
+	 * @return        The updated {@code event}
+	 * @throws        StudyUpException 
 	 */
-	public Event updateEvent(Event event);
+	public Event updateEventName(int eventID, String name) throws StudyUpException;
+	
 
 	/**
-	 * @return The list of all active {@code events}. i.e., the events whose date is
-	 *         in the future.
+	 * Adds a student to a specific event and throws StudyUpException incase of any 
+	 * constraint failures.
+	 * @param student The {@link Student} to be added.
+	 * @param eventID The {@code eventID} of the specific {@link Event} to be
+	 *                updated.
+	 * @return Event, if the student is added successfully.
+	 * @throws StudyUpException 
+	 */
+	public Event addStudentToEvent(Student student, int eventID) throws StudyUpException;
+
+	/**
+	 * Fetches all the active events present. i.e., the events whose {@code Date} is
+	 * in the future.
+	 * @return The list of all active {@code events}. 
 	 */
 	public List<Event> getActiveEvents();
 
 	/**
-	 * @return The list of all past {@code events}. i.e., the events with date <
-	 *         today.
+	 * Fetches all the past events. i.e., the events with past {@code Date}.
+	 * @return The list of all past {@code events}.
 	 */
 	public List<Event> getPastEvents();
 
-//	/**
-//	 * @return The list of present {@code events}. i.e., the events with date as
-//	 *         today.
-//	 */
-//	public List<Event> getCurrentEvents();
-
-	/**
-	 * @param student The {@link Student} to be added.
-	 * @param eventID The {@code eventID} of the specific {@link Event} to be
-	 *                updated.
-	 * @return {@code True}, if the student is added else {@code False}.
-	 * @throws Exception 
-	 */
-	public boolean addStudentToEvent(Student student, int eventID) throws Exception;
-
 	/**
 	 * @param eventID The {@code eventID} of the specific {@link Event} to delete.
-	 * @return {@code True}, if the event exists and is deleted, else {@code False}.
+	 * @return {@code Event}, if the event exists and is deleted, else null.
 	 */
-	public boolean deleteEvent(int eventID);
+	public Event deleteEvent(int eventID);
 
 }
--- src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
+++ src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
@@ -62,21 +62,18 @@ class EventServiceImplTest {
 	}
 
 	@Test
-	void testUpdateEventName_GoodCase() {
-		Event event = new Event();
-		event.setEventID(1);
-		event.setName("Renamed Event 1");
-		eventServiceImpl.updateEvent(event);
-		assertEquals("Renamed Event 1", DataStorage.eventData.get(event.getEventID()).getName());
+	void testUpdateEventName_GoodCase() throws StudyUpException {
+		int eventID = 1;
+		eventServiceImpl.updateEventName(eventID, "Renamed Event 1");
+		assertEquals("Renamed Event 1", DataStorage.eventData.get(eventID).getName());
 	}
 	
 	@Test
-	@Disabled
-	void testUpdateEvent_badCase() {
-		Event event = null;
+	void testUpdateEvent_WrongEventID_badCase() {
+		int eventID = 3;
 		Assertions.assertThrows(StudyUpException.class, () -> {
-			eventServiceImpl.updateEvent(event);
+			eventServiceImpl.updateEventName(eventID, "Renamed Event 3");
 		  });
 	}
-
+	
 }
